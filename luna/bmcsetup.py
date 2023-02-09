742240a14b6651807590767b9966f030d3fc75f8
Sumit Sharma <sumit.sharma@clustervision.com>
2023-02-09 11:50:21 +0100

    TRIX-87 luna bmcsetup update, inquiry select

--- luna/bmcsetup.py
+++ luna/bmcsetup.py
@@ -75,17 +75,16 @@ class BMCSetup(object):
         cmd.add_argument('--unmanaged_bmc_users', '-ubu', help='Unmanaged BMC Users')
         cmd.add_argument('--comment', '-c', help='Comment for BMC Setup')
         ## >>>>>>> BMC Setup Command >>>>>>> update
-        cmd = bmcsetup_args.add_parser('update', help='Update BMC Setup')
-        cmd.add_argument('name', help='Name of the BMC Setup')
-        cmd.add_argument('--network', '-N', metavar='N.N.N.N', help='BMC Setup')
-        cmd.add_argument('--prefix', '-P', metavar='PP', type=int, help='Prefix')
-        cmd.add_argument('--reserve', '-R', metavar='X.X.X.X', help='Reserve IP')
-        cmd.add_argument('--release', metavar='X.X.X.X', help='Release IP')
-        cmd.add_argument('--nshostname', help='Name server for zone file')
-        cmd.add_argument('--nsipaddress', metavar='N.N.N.N', help='Name server\'s IP for zone file')
-        cmd.add_argument('--include', action='store_true', help='Include data for zone file')
-        cmd.add_argument('--rev_include', action='store_true', help='Include data for reverse zone')
-        cmd.add_argument('--comment', '-C', action='store_true', help='Add comment')
+        cmd = bmcsetup_args.add_parser('update', help='Update a BMC Setup')
+        cmd.add_argument('--init', '-i', action='store_true', help='BMC Setup values one-by-one')
+        cmd.add_argument('--name', '-n', help='Name of the BMC Setup')
+        cmd.add_argument('--userid', '-uid', type=int, help='UserID for BMC Setup')
+        cmd.add_argument('--username', '-u', help='Username for BMC Setup')
+        cmd.add_argument('--password', '-p', help='Password for BMC Setup')
+        cmd.add_argument('--netchannel', '-nc', type=int, help='Net Channel for BMC Setup')
+        cmd.add_argument('--mgmtchannel', '-mc', type=int, help='MGMT Channel for BMC Setup')
+        cmd.add_argument('--unmanaged_bmc_users', '-ubu', help='Unmanaged BMC Users')
+        cmd.add_argument('--comment', '-c', help='Comment for BMC Setup')
         ## >>>>>>> BMC Setup Command >>>>>>> clone
         cmd = bmcsetup_args.add_parser('clone', help='Clone BMC Setup')
         cmd.add_argument('name', help='Name of the BMC Setup')
@@ -159,7 +158,7 @@ class BMCSetup(object):
             payload['name'] = Inquiry().ask_text("Kindly provide BMC Name")
             payload['userid'] = Inquiry().ask_number("Kindly provide BMC User ID")
             payload['username'] = Inquiry().ask_text("Kindly provide BMC Username")
-            payload['password'] = Inquiry().ask_text("Kindly provide BMC Password")
+            payload['password'] = Inquiry().ask_secret("Kindly provide BMC Password")
             payload['netchannel'] = Inquiry().ask_number("Kindly provide NET Channel")
             payload['mgmtchannel'] = Inquiry().ask_number("Kindly provide MGMT Channel")
             payload['unmanaged_bmc_users'] = Inquiry().ask_text("Provide Unmanaged BMC Users")
@@ -199,17 +198,66 @@ class BMCSetup(object):
         return True
 
 
-    def delete_bmcsetup(self, args=None):
-        """
-        Method to delete a bmc setup in Luna Configuration.
-        """
-        print(args)
-        return True
-
-
     def update_bmcsetup(self, args=None):
         """
         Method to update a bmc setup in Luna Configuration.
+        """
+        payload = {}
+        if args['init']:
+            get_list = Helper().get_list(self.table)
+            if get_list:
+                names = list(get_list['config']['bmcsetup'].keys())
+                payload['name'] = Inquiry().ask_select("Select BMC Setup to update", names)
+                payload['userid'] = Inquiry().ask_number("Update BMC User ID", True)
+                payload['username'] = Inquiry().ask_text("Update BMC Username", True)
+                payload['password'] = Inquiry().ask_secret("Update BMC Password", True)
+                payload['netchannel'] = Inquiry().ask_number("Update NET Channel", True)
+                payload['mgmtchannel'] = Inquiry().ask_number("Update MGMT Channel", True)
+                payload['unmanaged_bmc_users'] = Inquiry().ask_text("Update Unmanaged BMC Users", True)
+                comment = Inquiry().ask_confirm("Do you want to provide a comment?")
+                if comment:
+                    payload['comment'] = Inquiry().ask_text("Kindly provide comment(if any)", True)
+                filtered = {k: v for k, v in payload.items() if v != ''}
+                payload.clear()
+                payload.update(filtered)
+                if len(payload) != 1:
+                    fields, rows  = Helper().filter_data_col(self.table, payload)
+                    title = f'{self.table.capitalize()} Adding => {payload["name"]}'
+                    Presenter().show_table_col(title, fields, rows)
+                    confirm = Inquiry().ask_confirm(f'Add {payload["name"]} in {self.table.capitalize()}?')
+                    if not confirm:
+                        Helper().show_error(f'Add {payload["name"]} into {self.table.capitalize()} Aborted')
+            else:
+                response = Helper().show_error(f'No {self.table.capitalize()} is available.')
+        else:
+            del args['debug']
+            del args['command']
+            del args['action']
+            del args['init']
+            payload = args
+        if len(payload) != 1:
+            request_data = {}
+            request_data['config'] = {}
+            request_data['config'][self.table] = {}
+            request_data['config'][self.table][payload['name']] = payload
+            get_list = Helper().get_list(self.table)
+            if get_list:
+                names = list(get_list['config']['bmcsetup'].keys())
+                if payload["name"] in names:
+                    response = Rest().post_data(self.table, payload['name'], request_data)
+                    if response == 204:
+                        Helper().show_success(f'{self.table.capitalize()}, {payload["name"]} updated.')
+                else:
+                    Helper().show_error(f'{payload["name"]} Not found in {self.table.capitalize()}.')
+            else:
+                response = Helper().show_error(f'No {self.table.capitalize()} is available.')
+        else:
+            Helper().show_error(f'Nothing to update in {payload["name"]}.')
+        return True
+
+    def delete_bmcsetup(self, args=None):
+        """
+        Method to delete a bmc setup in Luna Configuration.
         """
         print(args)
         return True
--- luna/utils/inquiry.py
+++ luna/utils/inquiry.py
@@ -14,7 +14,6 @@ __status__      = "Production"
 
 import sys
 from InquirerPy import inquirer, get_style
-from InquirerPy.validator import NumberValidator
 from termcolor import colored
 
 class Inquiry(object):
@@ -36,12 +35,12 @@ class Inquiry(object):
             }
         self.style = get_style(self.styledict, style_override=False)
 
-    def ask_text(self, message=None):
+    def ask_text(self, message=None, update=None):
         """
         This method will ask a text based question
         and revert with key value based answer.
         """
-        def ask(count, message):
+        def ask(count, message, update):
             if count == 3:
                 error_message = "You have lost all 3 attempts, Please try again."
                 print(colored(error_message, 'red', attrs=['bold']))
@@ -49,46 +48,51 @@ class Inquiry(object):
             else:
                 try:
                     name = inquirer.text(message=message, style= self.style).execute()
+                    if update:
+                        return name
                     if not name:
                         print(colored("Try Again!", 'yellow', attrs=['bold']))
                         count = count +1
-                        return ask(count, message)
+                        return ask(count, message, update)
                     else:
                         return name
                 except KeyboardInterrupt:
                     print(colored("Exited! Try Again, with new Inputs.", 'red', attrs=['bold']))
                     sys.exit(0)
-        response = ask(0, message)
+        response = ask(0, message, update)
         return response
 
 
-    def ask_number(self, message=None):
+    def ask_number(self, message=None, update=None):
         """
         This method will ask a text based question
         and revert with key value based answer.
         """
-        def ask(count, message):
+        def ask(count, message, update):
             if count == 3:
                 error_message = "You have lost all 3 attempts, Please try again."
                 print(colored(error_message, 'red', attrs=['bold']))
                 sys.exit(0)
             else:
                 try:
-                    name = inquirer.text(
+                    name = inquirer.number(
                         message = message,
                         style = self.style,
-                        validate = NumberValidator()
+                        # validate = NumberValidator(),
+                        default = None
                     ).execute()
+                    if update:
+                        return name
                     if not name:
                         print(colored("Try Again!", 'yellow', attrs=['bold']))
                         count = count +1
-                        return ask(count, message)
+                        return ask(count, message, update)
                     else:
                         return name
                 except KeyboardInterrupt:
                     print(colored("Exited! Try Again, with new Inputs.", 'red', attrs=['bold']))
                     sys.exit(0)
-        response = ask(0, message)
+        response = ask(0, message, update)
         return response
 
 
@@ -100,6 +104,57 @@ class Inquiry(object):
         response = False
         try:
             response = inquirer.confirm(message=message, style= self.style, default=False).execute()
+        except KeyboardInterrupt:
+            print(colored("Exited! Try Again, with new Inputs.", 'red', attrs=['bold']))
+            sys.exit(0)
+        return response
+
+
+    def ask_secret(self, message=None, update=None):
+        """
+        This method will ask a text based question
+        and revert with key value based answer.
+        """
+        def ask(count, message, update):
+            if count == 3:
+                error_message = "You have lost all 3 attempts, Please try again."
+                print(colored(error_message, 'red', attrs=['bold']))
+                sys.exit(0)
+            else:
+                try:
+                    name = inquirer.secret(
+                        message = message,
+                        style = self.style,
+                        transformer=lambda _: "[hidden]"
+                    ).execute()
+                    if update:
+                        return name
+                    if not name:
+                        print(colored("Try Again!", 'yellow', attrs=['bold']))
+                        count = count +1
+                        return ask(count, message, update)
+                    else:
+                        return name
+                except KeyboardInterrupt:
+                    print(colored("Exited! Try Again, with new Inputs.", 'red', attrs=['bold']))
+                    sys.exit(0)
+        response = ask(0, message, update)
+        return response
+
+
+    def ask_select(self, message=None, choices=None):
+        """
+        This method will provide some choices to
+        select the desired input.
+        """
+        response = False
+        try:
+            response = inquirer.select(
+                message = message,
+                style = self.style,
+                choices = choices,
+                default = None,
+            ).execute()
         except KeyboardInterrupt:
             print(colored("Exited! Try Again, with new Inputs.", 'red', attrs=['bold']))
             sys.exit(0)
