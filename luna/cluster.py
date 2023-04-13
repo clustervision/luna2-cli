13716bc6ad59ff5c32ee57924405f977c2ebc334
Sumit Sharma <sumit.sharma@clustervision.com>
2023-04-13 14:13:40 +0200

    TRIX-88 cluster show and change methods updated

--- luna/cluster.py
+++ luna/cluster.py
@@ -13,8 +13,6 @@ __email__       = "sumit.sharma@clustervision.com"
 __status__      = "Development"
 
 from luna.utils.helper import Helper
-from luna.utils.presenter import Presenter
-from luna.utils.rest import Rest
 from luna.utils.log import Log
 
 class Cluster():
@@ -27,9 +25,7 @@ class Cluster():
         self.logger = Log.get_logger()
         self.args = args
         self.table = "cluster"
-        self.get_list = None
         if self.args:
-            self.get_list = Rest().get_data(self.table)
             if self.args["action"] is None:
                 self.cluster_info()
             elif self.args["action"] == 'change':
@@ -71,40 +67,11 @@ class Cluster():
         """
         This method to show the cluster information.
         """
-        self.logger.debug(f'Get List Data from Helper => {self.get_list}')
-        if self.get_list:
-            data = self.get_list['config'][self.table]
-            if self.args['raw']:
-                json_data = Helper().prepare_json(data)
-                response = Presenter().show_json(json_data)
-            else:
-                fields, rows  = Helper().filter_data_col(self.table, data)
-                self.logger.debug(f'Fields => {fields}')
-                self.logger.debug(f'Rows => {rows}')
-                title = f'{self.table.capitalize()} => {data["name"]}'
-                response = Presenter().show_table_col(title, fields, rows)
-        else:
-            response = Helper().show_error('No Cluster Found.')
-        return response
+        return Helper().show_data(self.table, self.args)
 
 
     def change_cluster(self):
         """
         This method update the luna cluster.
         """
-        for remove in ['verbose', 'command', 'action', 'raw']:
-            self.args.pop(remove, None)
-        payload = Helper().prepare_payload(self.args)
-        if payload:
-            request_data = {'config': {self.table: payload}}
-            self.logger.debug(f'Payload => {request_data}')
-            response = Rest().post_data(self.table, None, request_data)
-            self.logger.debug(f'Response => {response}')
-            if response.status_code == 204:
-                Helper().show_success(f'{self.table.capitalize()} is updated.')
-            else:
-                Helper().show_error(f'HTTP Error Code {response.status_code}.')
-                Helper().show_error(f'HTTP Error {response.content}.')
-        else:
-            Helper().show_warning('Provide Something to update.')
-        return True
+        return Helper().update_record(self.table, self.args)
