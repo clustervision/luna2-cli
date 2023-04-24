082988622cef897e51aed711673d5135159d7243
Sumit Sharma <sumit.sharma@clustervision.com>
2023-04-24 16:46:25 +0200

    TRIX-88 Version File fullpath

--- luna/cli.py
+++ luna/cli.py
@@ -110,7 +110,10 @@ class Cli():
     def get_version(self):
         """This Method will fetch the current version of Luna CLI from VERSION File."""
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        base_dir = str(Path(current_dir).parent)
+        if 'luna' == sys.argv[0]:
+            base_dir = current_dir
+        else:
+            base_dir = str(Path(current_dir).parent)
         version_file = f'{base_dir}/{VERSION_FILE}'
         with open(version_file, 'r', encoding='utf-8') as ver:
             version = ver.read()
